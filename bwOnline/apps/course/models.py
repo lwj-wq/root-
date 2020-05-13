f57fbc093711512fb58cdd7385fdc5b5abce00f0
Bruce <1556181289@qq.com>
2020-05-13 20:12:36 +0800

    '课程章节六'

--- bwOnline/apps/course/models.py
+++ bwOnline/apps/course/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from organization.models import CourseOrg
+from organization.models import CourseOrg,Teacher
 
 # 课程信息表
 from django.db import models
@@ -20,9 +20,13 @@ class Course(models.Model):
     fav_nums = models.IntegerField("收藏人数",default=0)
     image = models.ImageField("封面图",upload_to="courses/%Y/%m",max_length=100)
     click_nums = models.IntegerField("点击数",default=0)
+    tag = models.CharField('课程标签', default='', max_length=10)
     add_time = models.DateTimeField("添加时间",default=datetime.now,)
     course_org = models.ForeignKey(CourseOrg, on_delete=models.CASCADE, verbose_name="所属机构", null=True, blank=True)
     category = models.CharField("课程类别",max_length=20, default="")
+    teacher = models.ForeignKey(Teacher, verbose_name='讲师', null=True, blank=True, on_delete=models.CASCADE)
+    youneed_know = models.CharField('课程须知', max_length=300, default='')
+    teacher_tell = models.CharField('老师告诉你', max_length=300, default='')
 
     class Meta:
         verbose_name = "课程"
@@ -31,6 +35,10 @@ class Course(models.Model):
     def get_zj_nums(self):
         #获取课程的章节数
         return self.lesson_set.all().count()
+
+    def get_course_lesson(self):
+        # 获取课程的章节
+        return self.lesson_set.all()
 
     def get_learn_users(self):
         #获取这门课程的学习用户
@@ -41,29 +49,36 @@ class Course(models.Model):
 
 # 章节
 class Lesson(models.Model):
-    # 因为一个课程对应很多章节。所以在章节表中将课程设置为外键。
-    # 作为一个字段来让我们可以知道这个章节对应那个课程
-    course = models.ForeignKey(Course, verbose_name=u"课程", on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name=u"章节名")
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+    '''课程章节'''
+    course = models.ForeignKey(Course,verbose_name='课程',on_delete=models.CASCADE)
+    name = models.CharField("章节名",max_length=100)
+    add_time = models.DateTimeField("添加时间",default=datetime.now)
 
     class Meta:
-        verbose_name = u"章节"
+        verbose_name = "章节"
         verbose_name_plural = verbose_name
 
+    def get_lesson_vedio(self):
+        #获取章节所有视频
+        return self.video_set.all()
+
+    def __str__(self):
+        return '《{0}》课程的章节 >> {1}'.format(self.course, self.name)
 
 # 每章视频
 class Video(models.Model):
-    # 因为一个章节对应很多视频。所以在视频表中将章节设置为外键。
-    # 作为一个字段来存储让我们可以知道这个视频对应哪个章节.
-    lesson = models.ForeignKey(Lesson, verbose_name=u"章节", on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name=u"视频名")
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+    lesson = models.ForeignKey(Lesson, verbose_name="章节",on_delete=models.CASCADE)
+    name = models.CharField("视频名",max_length=100)
+    url = models.CharField('访问地址',default='',max_length=200)
+    learn_times = models.IntegerField("学习时长(分钟数)", default=0)
+    add_time = models.DateTimeField("添加时间", default=datetime.now)
 
     class Meta:
-        verbose_name = u"视频"
+        verbose_name = "视频"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
 
 # 课程资源
 class CourseResource(models.Model):
--- bwOnline/apps/course/ursl.py
+++ bwOnline/apps/course/ursl.py
@@ -7,4 +7,12 @@ app_name = "course"
 urlpatterns = [
     path('list/',CourseListView.as_view(),name='course_list'),
     re_path('course/(?P<course_id>\d+)/', CourseDetailView.as_view(), name="course_detail"),
+    # 课程章节信息页
+    re_path('info/(?P<course_id>\d+)/', CourseInfoView.as_view(), name="course_info"),
+    #课程评论
+    re_path('comment/(?P<course_id>\d+)/', CommentsView.as_view(), name="course_comments"),
+    #添加评论
+    path('add_comment/', AddCommentsView.as_view(), name="add_comment"),
+    # 课程视频播放页
+    path('video/(?P<video_id>\d+)/', VideoPlayView.as_view(), name="video_play"),
 ]
